cf96cd5c40f0decf6a8134da78c7241e954b5303
jansttlmr <jansettelmeier@gmail.com>
2025-02-16 19:16:34 +0100

    adding screenshots

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ from starter.ml import model
 from starter.ml import data
 import os
 
-if "DYNO" in os.environ and os.path.isdir(".dvc"):
-    os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
-        exit("dvc pull failed")
-    os.system("rm -r .dvc .apt/usr/lib/dvc")
-
 cat_features = [
     "workclass",
     "education",
