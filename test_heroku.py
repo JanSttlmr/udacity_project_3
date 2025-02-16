ec22fc97b67a6d9a7dc33362f7edd9a088b12a96
jansttlmr <jansettelmeier@gmail.com>
2025-02-16 18:37:32 +0100

    screenshots and test app

---
+++ test_heroku.py
@@ -0,0 +1,19 @@
+import requests
+df = {
+    "age": 30,
+    "workclass": "State-gov",
+    "education": "Masters",
+    "maritalStatus": "Never-married",
+    "occupation": "Tech-support",
+    "relationship": "nmarried",
+    "race": "White",
+    "sex": "Male",
+    "hoursPerWeek": 20,
+    "nativeCountry": "Germany"
+    }
+r = requests.post('https://project-3-jan-c1491110d0b5.herokuapp.com', json=df)
+
+assert r.status_code == 200
+
+print("status_code: " + str(r.status_code))
+print("response: " + str(r.json()))
